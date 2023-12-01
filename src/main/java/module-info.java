796e61946a02b2d140d25c076d89cd6c03b4a5be
mostafahakim03 <mostafahakim02@gmail.com>
2023-12-01 14:49:18 +0200

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.travel_managment_system.travel_managment_system {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.travel_managment_system.travel_managment_system to javafx.fxml;
+    exports com.travel_managment_system.travel_managment_system;
+}
