ac9b033810498ebd367f3430887f505b28ad98c5
melwensh <melwensh100200@gmail.com>
2023-12-02 16:24:47 +0200

    parent1

--- src/main/java/com/travel_managment_system/travel_managment_system/TMSApplication.java
+++ src/main/java/com/travel_managment_system/travel_managment_system/TMSApplication.java
@@ -2,6 +2,7 @@ package com.travel_managment_system.travel_managment_system;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,11 +11,10 @@ import java.io.IOException;
 public class TMSApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TMSApplication.class.getResource("Homepage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-       stage.setTitle("Hello!");
-      stage.setScene(scene);
-     stage.show();
+        Parent root= FXMLLoader.load(getClass().getResource("Homepage.fxml"));
+        Scene scene =new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
