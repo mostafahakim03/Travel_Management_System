ff0c682907b54ae57091c57db067d6d439009438
melwensh <melwensh100200@gmail.com>
2023-12-02 16:28:25 +0200

    parent1

--- src/main/java/com/travel_managment_system/travel_managment_system/User/User.java
+++ src/main/java/com/travel_managment_system/travel_managment_system/User/User.java
@@ -1,4 +1,12 @@
 package com.travel_managment_system.travel_managment_system.User;
 
-public class User {
+public abstract class User {
+    String name;
+    String username;
+    private String pass;
+    String phone;
+
+    int age;
+
+    public abstract void display();
 }
