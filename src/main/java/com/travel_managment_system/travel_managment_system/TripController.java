f4a5f079f5b420dbcf3c71275ee6805aa151806c
Menna Mohamed <2022170446@cis.asu.edu.eg>
2023-12-05 21:00:23 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/travel_managment_system/travel_managment_system/TripController.java
+++ src/main/java/com/travel_managment_system/travel_managment_system/TripController.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseDragEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -45,42 +46,31 @@ public class TripController implements Initializable {
     @FXML
     private String[] packages = {"Silver", "Golden" , "Platinum"};
 
-    Trip trip;
-
     public void ViewTripDetails(Trip trip){
         TripNameLabel.setText(trip.tripName);
         tripTypeLabel.setText(trip.tripType);
         start_dateLabel.setText(trip.start_date);
         end_dateLabel.setText(trip.end_date);
         priceLabel.setText(String.valueOf(trip.price));
-
-//        this.trip.tripName = trip.tripName;
-//        this.trip.trip_id = trip.trip_id;
-//        this.trip.tripType = trip.tripType;
-//        this.trip.start_date = trip.start_date;
-//        this.trip.end_date = trip.end_date;
-//        this.trip.price = trip.price;
-//        this.trip.payment = trip.payment;
-//        this.trip.tripImage = trip.tripImage;
-////        Trip.numbersOfSeats = trip.Seats;
-//        Trip.noOfTrips++;
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         packageTypeChoice.getItems().addAll(packages);
         packageTypeChoice.setOnAction(this::getPackage);
     }
+
     public void getPackage(ActionEvent event){
-        trip.packageType = String.valueOf(packageTypeChoice.getValue());
-        ChoiceLabel.setText("You chose : " + trip.packageType);
-        if (trip.packageType.equals("Silver")) {
+        String packageType = String.valueOf(packageTypeChoice.getValue());
+        ChoiceLabel.setText("You chose : " + packageType);
+        if (packageType.equals("Silver")) {
             PackageMessageLabel.setText("The trip contains only the transportation and half-board");
-        } else if (trip.packageType.equals("Golden")) {
+        } else if (packageType.equals("Golden")) {
             PackageMessageLabel.setText("The trip contains the transportation and full-board");
-        } else if (trip.packageType.equals("Platinum")) {
+        } else if (packageType.equals("Platinum")) {
             PackageMessageLabel.setText("The trip contains the transportation, full-board and activities");
         }
     }
+
     public void logoutButtonClicked(ActionEvent event) throws IOException {
         System.out.println("Logout pending!");
         //alert code
@@ -117,6 +107,22 @@ public class TripController implements Initializable {
         stage.setScene(scene);
         stage.show();
         TripHome.getScene().getWindow().hide();
+
+    }
+
+
+
+    public void Booking(ActionEvent actionEvent) {
+
+        int numbersOfTickets = Integer.parseInt(numbersOfTicketsInputs.getText());
+
+        if (tripTypeLabel.getText().equals("family") && numbersOfTickets < 3){
+            NoOfTicketsMessageLabel1.setText("The minimum number for the tickets is 3");
+        }
+        else if (tripTypeLabel.getText().equals("couple") && numbersOfTickets % 2 != 0){
+            NoOfTicketsMessageLabel1.setText("Tickets must be Even number");
+        } else
+            NoOfTicketsMessageLabel1.setText("");
 
     }
 
