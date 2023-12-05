685a3f42f74e89765db02a4ae050d7504ec48705
Menna Mohamed <2022170446@cis.asu.edu.eg>
2023-12-05 19:48:07 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/travel_managment_system/travel_managment_system/TripController.java
+++ src/main/java/com/travel_managment_system/travel_managment_system/TripController.java
@@ -2,14 +2,16 @@ package com.travel_managment_system.travel_managment_system;
 import com.travel_managment_system.travel_managment_system.Trip.Trip;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -79,4 +81,43 @@ public class TripController implements Initializable {
             PackageMessageLabel.setText("The trip contains the transportation, full-board and activities");
         }
     }
+    public void logoutButtonClicked(ActionEvent event) throws IOException {
+        System.out.println("Logout pending!");
+        //alert code
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You are logging out...");
+        alert.setContentText("Are you sure you want to logout?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            //logging out code
+            Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+            TripHome.getScene().getWindow().hide();
+        }
+    }
+
+    public void THomeClicked(ActionEvent event) throws IOException {
+        System.out.println("Going home!");
+        Parent root = FXMLLoader.load(getClass().getResource("THomepage.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+        TripHome.getScene().getWindow().hide();
+    }
+
+    public void TProfileClicked(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Tprofile.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+        TripHome.getScene().getWindow().hide();
+
+    }
+
 }
