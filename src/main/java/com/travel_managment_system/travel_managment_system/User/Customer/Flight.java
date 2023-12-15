ac7008d67e116b63f228009ab6aa2c34a50d5661
Menna Mohamed <2022170446@cis.asu.edu.eg>
2023-12-15 03:54:18 +0200

    Cleaned files

--- src/main/java/com/travel_managment_system/travel_managment_system/User/Customer/Flight.java
+++ src/main/java/com/travel_managment_system/travel_managment_system/User/Customer/Flight.java
@@ -4,10 +4,4 @@ public class Flight {
     private String FlightCompanyName;
     public int [] arrNumberOfSeats = new int[50];
     public Boolean [] isReserved=new Boolean[50];
-    public void FillarrSeats(){
-        for(int i =0; i<50;i++) {
-            arrNumberOfSeats[i] = i+1;
-            isReserved[i]=false;
-        }
-    }
 }
